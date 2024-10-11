26ae95eacc95c32fe4b877b88229ad28bf717fe9
casa_ <nataliamoragarcia24@gmail.com>
2024-10-10 19:22:40 -0600

    poonybus

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,33 +5,37 @@ public class Usuario {
     // Atributos
     private String nombre;
     private int id_Estudiante;
-    private String tipo_de_persona;
+    private String tipo_de_usuario;
     private String ruta_asignada;
     private int saldo;
 
-    // Constructor para crear objetos y darle valores iniciales a los objetos
-    public Usuario(){
+    // Constructor para darle valores iniciales a los objetos
+    public Usuario() {
         this.nombre = nombre;
         this.id_Estudiante = id_Estudiante;
-        this.tipo_de_persona = tipo_de_persona;
+        this.tipo_de_usuario = tipo_de_usuario;
         this.ruta_asignada = ruta_asignada;
-        this.saldo =saldo;
+        this.saldo = saldo;
     }
 
     // Metodos
-    public void mostrarDetalles(){
+    public void mostrarDetalles() {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Nombre: ");
         String nombre = entrada.nextLine();
+
         System.out.println("ID Estudiante: ");
         int id_Estudiante = entrada.nextInt();
+
         System.out.println("Tipo de usuario: ");
         entrada.nextLine();     //despues de nextInt queda un salto de línea en el buffer de entrada y nextLine lee ese salto de linea y no recibe una entrada
-        String tipo_de_persona = entrada.nextLine();
+        String tipo_de_usuario = entrada.nextLine();
+
         System.out.println("Ruta asignada: ");
         String ruta_asignada = entrada.nextLine();
-        System.out.println("Saldo: $");
+
+        System.out.println("Saldo: ");
         int saldo = entrada.nextInt();
 
     }
