6ab4d84c094c37a413be9201528920f606e00e34
casa_ <nataliamoragarcia24@gmail.com>
2024-10-10 18:56:30 -0600

    PonyBus

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,43 +1,38 @@
+import java.util.Scanner;
+
 public class Usuario {
 
     // Atributos
     private String nombre;
     private int id_Estudiante;
-    private String tipo_de_usuario;
+    private String tipo_de_persona;
     private String ruta_asignada;
     private int saldo;
 
     // Constructor para crear objetos y darle valores iniciales a los objetos
-    public Usuario(String nombre, int id_Estudiante, String tipo_de_usuario, String ruta_asignada, int saldo){
+    public Usuario(){
         this.nombre = nombre;
         this.id_Estudiante = id_Estudiante;
-        this.tipo_de_usuario = tipo_de_usuario;
+        this.tipo_de_persona = tipo_de_persona;
         this.ruta_asignada = ruta_asignada;
         this.saldo =saldo;
     }
 
     // Metodos
     public void mostrarDetalles(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("ID Estudiante: " + id_Estudiante);
-        System.out.println("Tipo de usuario: " + tipo_de_usuario);
-        System.out.println("Ruta asignada: " + ruta_asignada);
-        System.out.println("Saldo: $" + saldo);
-    }
-    public void recargarSaldo(int monto){
-        if (monto > 0) {
-            saldo += monto;
-            System.out.println("Recarga exitosa. Saldo actual: $" + saldo);
-        } else {
-            System.out.println("Monto inválido para recargar.");
-        }
-    }
-    public void usarTransporte(int costo) {
-        if (saldo >= costo) {
-            saldo -= costo;
-            System.out.println("Transporte utilizado. Costo: $" + costo + ". Saldo restante: $" + saldo);
-        } else {
-            System.out.println("Saldo insuficiente.");
-        }
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.println("ID Estudiante: ");
+        int id_Estudiante = entrada.nextInt();
+        System.out.println("Tipo de usuario: ");
+        entrada.nextLine();     //despues de nextInt queda un salto de línea en el buffer de entrada y nextLine lee ese salto de linea y no recibe una entrada
+        String tipo_de_persona = entrada.nextLine();
+        System.out.println("Ruta asignada: ");
+        String ruta_asignada = entrada.nextLine();
+        System.out.println("Saldo: $");
+        int saldo = entrada.nextInt();
+
     }
 }
